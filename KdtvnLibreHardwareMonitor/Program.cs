8b629fc37882a37a0a13f38348402576129b16b3
Hiếu Hoàng <blueblue.hhp@gmail.com>
2025-07-18 10:43:43 +0700

    thêm project để chạy bằng Console

---
+++ KdtvnLibreHardwareMonitor/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using LibreHardwareMonitor.Hardware;
+
+
+namespace KdtvnLibreHardwareMonitor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KdtvnLibreHardwareMonitor is running...");
+
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true
+            };
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+
+            string report = computer.GetReport();
+            computer.Close();
+
+            string directory = AppDomain.CurrentDomain.BaseDirectory; // Lấy thư mục hiện tại của file .exe
+            string filename = $"KdtvnLibreHardwareMonitorLog.txt";
+            string fullPath = Path.Combine(directory, filename);
+
+            try
+            {
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+                File.WriteAllText(fullPath, report);
+                Console.WriteLine($"Đã lưu file log vào: {fullPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi khi lưu file log: {ex.Message}");
+            }
+
+
+        }
+    }
+
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
+
+}
---
+++ KdtvnLibreHardwareMonitor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KdtvnLibreHardwareMonitor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KdtvnLibreHardwareMonitor")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("460b7989-a153-485b-8c2d-3e4a03c5a69c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
